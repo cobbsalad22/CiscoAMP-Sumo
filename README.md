007e5409b51e8a9fb80e11f898ee12b86db7930c
Jacob Grant <jacobgrant@es-mbp-sajacob.local>
2019-03-12 12:44:19 -0400

    Initial script commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # CiscoAMP-Sumo
 Script to pull events from Cisco AMP and forward them to an HTTP sumo collector
+
+First run the create_event_stream.py from the CiscoSecuritty Github account found here at time of writing:
+https://github.com/Ciscosecurity/amp-04-event-stream-creator
+
+This should provide you with the necessary details for configuring this script, similar to below:
+
+AMQP Credentials:
+User Name:..... [API_USER]
+Password:...... [API_PASS]
+Host:.......... export-streaming.amp.cisco.com
+Port:.......... 443
+Queue Name:.... [QUEUE_NAME]
+
+Edit the configuration portion of the amp-sumo.py script with these values, then run
---
+++ amp-sumo.py
@@ -0,0 +1,35 @@
+import pika
+from time import sleep
+import requests
+import simplejson as json
+
+## Configuration
+#url = 'SUMO_URL_HERE'
+#apiuser = 'API_USER'
+#queuename = 'QUEUE_NAME'
+#apipass = 'PASSWORD'
+#headers = {'Content-type': 'application/json'}
+
+#connection = pika.BlockingConnection() # pass in the BlockingConnection that includes a connection url
+print("before connection")
+connection = pika.BlockingConnection(pika.ConnectionParameters(host="export-streaming.amp.cisco.com", port=443,credentials=pika.PlainCredentials(username=apiuser, password=apipass), ssl=True))
+print("after connection")
+channel = connection.channel()
+print("after channel")
+
+while True:
+  try:
+   method_frame, header_frame, body = channel.basic_get(queuename)
+   print("after channel.basic_get")
+   if method_frame:
+    print(body)
+    r = requests.post(url, data=body, headers=headers)
+    channel.basic_ack(method_frame.delivery_tag)
+   else:
+    print('No message returned')
+#    print("Sleeping for 5s")
+    sleep(5)
+  except KeyboardInterrupt:
+   print('Exiting')
+   break
+  continue
